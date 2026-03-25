53fc0c4f2074f56efe591625abfcf042fc85f3d4
ben.sanchez <ben.sanchez@mastercard.com>
2026-03-25 12:36:41 -0400

    files

--- zharing/main.py
+++ zharing/main.py
@@ -35,6 +35,70 @@ app_ = zonzo.Application.from_module(__name__)
 #         return v1_app(environ, start_response)
 #     return main_app(environ, start_response)
 
+import time
+
+import gevent
+
+TICK_RATE = 20
+TICK_DT = 1.0 / TICK_RATE
+
+Tick_global = 0
+
+
+def tick_loop():
+    global Tick_global
+    tick_next = time.monotonic()
+
+    while True:
+        time_real = time.monotonic()
+
+        if time_real < tick_next:
+            gevent.sleep(tick_next - time_real)
+
+        Tick_global += 1
+        step_simulation(tick)
+        tick_next += TICK_DT
+
+
+gevent.spawn(tick_loop)
+
+import collections
+
+dq_clients = collections.defaultdict(collections.deque)
+
+
+def handle_client_message(id_player, msg):
+    if msg['type'] == 'input':
+        dq_clients[id_player].append(msg)
+
+
+def sumulation_step(tick):
+    collect_inputs_from_clients()
+    update_world()
+    send_state_updates(tick)
+
+
+def collect_inputs():
+    for id_player, qu in dq_clients.items():
+        while qu:
+            msg_input = qu.popleft()
+            apply_input(id_player, msg_input)
+
+
+def send_state_updates(tick):
+    delta = compute_state_delta()
+
+    msg = {
+        'type': 'state_delta',
+        'tick': tick,
+        'payload': delta,
+        }
+
+    msg_encoded = json.dumps(msg)
+
+    for ws in clients:
+        ws.send(msg_encoded)
+
 
 def main():
     print("hola")
